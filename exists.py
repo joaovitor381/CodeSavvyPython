b5f44bf62d651f7821ffeb37de00b21713f3d264
João vitor <joaoviitor888@outlook.com>
2023-07-29 00:44:17 -0300

    co2

---
+++ exists.py
@@ -0,0 +1,8 @@
+import os
+
+# "exists" serve para identificar se a pasta ou arquivo existe
+print(os.path.exists("fatorial.py"))
+print(os.path.exists("boolean.py"))
+print(os.path.exists("primo.py"))
+print(os.path.exists("concatenacao.py"))
+print(os.path.exists("geradordesenhas.py"))
