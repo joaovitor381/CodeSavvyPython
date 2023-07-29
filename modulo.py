3a55110eeffb26da02b9e90cb2dfc8f7641a7e11
João vitor <joaoviitor888@outlook.com>
2023-07-29 00:47:10 -0300

    co2

---
+++ modulo.py
@@ -0,0 +1,7 @@
+# Import random
+from random import random, choice  # choice retorna um elemento aleatório
+
+print(random())
+
+lista = ["carbono", "nitrogenio", "oxigenio"]
+print(choice(lista))  # Usando "random" faz gerar valores aleatórios
